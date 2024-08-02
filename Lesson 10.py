383d62acda4870e5943a0a05d7af1d80f38a871c
Rodion <rodionbulgakov20@gmail.com>
2024-08-02 14:59:19 +0300

    add lesson 10

--- Lesson 10.py
+++ Lesson 10.py
@@ -1,19 +1,25 @@
 expenses_by_categories = {}
 expenses_by_users = {}
 with open('hw10.txt', 'rt', encoding='utf-8') as file:
-    content = file.read()
-lines = content.splitlines()
-for line in lines:
-    tmp = line.split()
-    if len(tmp) >= 4:
-        _, *name, money, category = tmp
-        name = ' '.join(name)
-        money = float(money.replace('$', ''))
-        expenses_by_categories[category] = expenses_by_categories.get(category, 0) + money
-        expenses_by_users[name] = expenses_by_users.get(name, 0) + money
-    else:
-        print(f"Ошибка в строке: {line}")
+    for line in file:
+        tmp = line.split()
+        if len(tmp) >= 4:
+            _, *name, money, category = tmp
+            name = ' '.join(name)
+            money = float(money.replace('$', ''))
+            if category in expenses_by_categories:
+                expenses_by_categories[category] += money
+            else:
+                expenses_by_categories[category] = money
+            if name in expenses_by_users:
+                expenses_by_users[name] += money
+            else:
+                expenses_by_users[name] = money
+        else:
+            print(f"Ошибка в строке: {line}")
+print("Расходы по категориям:")
 for category, money in expenses_by_categories.items():
     print(f'{category}: {money:.2f}$')
+print("\nРасходы по пользователям:")
 for user, money in expenses_by_users.items():
     print(f'{user}: {money:.2f}$')
